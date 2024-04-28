ef33230251b64c30ee58108cb552facae5fb86fc
Styven <styvenn16@gmail.com>
2024-04-28 17:40:21 -0300

    Criando o template, a view e a url para exibir contato único

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,19 @@
+from django.http import Http404
 from django.shortcuts import render
 from contact.models import Contact
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
     context = {'contacts':contacts,}
     return render(request,'contact/index.html',context)
+
+
+
+def contact(request, id):
+    single_contact = Contact.objects.filter(pk=id,show=True).first()
+
+    if single_contact is None:
+        raise Http404()
+    
+    context = {'contact':single_contact,}
+    return render(request,'contact/contact.html',context)
