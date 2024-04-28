e4169d2a4737ef599316c7ecb22b76d58db38f52
Styven <styvenn16@gmail.com>
2024-04-28 08:50:33 -0300

    criando o Model Category e fazendo a ligação com chave estrangeira

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -12,8 +16,10 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%D')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-    
+
+
