d882166dad54e0f869287b0c321e13dd106eba4b
Styven <styvenn16@gmail.com>
2024-04-29 06:46:51 -0300

    Criando o cabeçalho principal do site (header) e Criando arquivos parciais para usar com include nos templates

--- contact/views.py
+++ contact/views.py
@@ -4,16 +4,17 @@ from contact.models import Contact
 # Create your views here.
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
-    context = {'contacts':contacts,}
+    context = {'contacts':contacts,'titulo': 'Contatos - ' }
     return render(request,'contact/index.html',context)
 
 
 
 def contact(request, id):
     single_contact = Contact.objects.filter(pk=id,show=True).first()
+    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
 
     if single_contact is None:
         raise Http404()
     
-    context = {'contact':single_contact,}
+    context = {'contact':single_contact,'titulo':contact_name}
     return render(request,'contact/contact.html',context)
